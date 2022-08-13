8008cab687329e5161cf113b37de32a3106d1964
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-13 17:48:21 -0400

    checking functionality of removing users from db

--- routes/pet.js
+++ routes/pet.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const petController = require('../controllers/pet');
-const { ensureAuth, ensureGuest } = require('../middleware/auth');
+// const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-router.get('/', ensureAuth, petController.getPetPage);
+router.get('/', petController.getPetPage);
 
 module.exports = router;
