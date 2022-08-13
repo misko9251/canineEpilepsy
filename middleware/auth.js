18de0c888648e5d0b0a65170fafd36dc39b3a950
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-13 16:57:17 -0400

    added middleware to protect routes

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+    ensureAuth: function (req, res, next){
+        if(req.isAuthenticated()){
+            return next()
+        }else{
+            res.redirect('/')
+        }
+    },
+    ensureGuest: function (req, res, next){
+        if(req.isAuthenticated()){
+            res.redirect('/petProfile')
+        }
+    }
+}
+
+// ensureAuth will allow you to access pages that you need to be logged in to see, such as pet profile
+// ensureGuest will make it so logged in users cannot view pages that logged out users should see, such as the login page
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 // Auth with Google with a GET request
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
@@ -13,5 +14,14 @@ router.get('/google/callback',
     // Successful authentication, redirect home.
     res.redirect('/petProfile');
   });
+
+// Log Out User
+
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
 
 module.exports = router;
