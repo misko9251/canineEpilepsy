ec7c61e88357dfcd83a1de0e19094da1fed5ca51
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-12 22:42:23 -0400

    added google auth users to db

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// Auth with Google with a GET request
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+// Google auth callback
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.render('/petProfile');
+  });
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
 const homeRouter = require('./routes/home');
+const connectDB = require('./config/db');
 
 require('dotenv').config({path: './config/.env'});
+
+// Passport
+require('./config/passport')(passport);
+
+connectDB()
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', homeRouter);
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
 
-app.listen(process.env.PORT, () =>{
+// Set Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', homeRouter);
+app.use('/auth', require('./routes/auth'));
+
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log(`Listening on Port ${process.env.PORT}`)
 })
