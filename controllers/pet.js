c558bf5961f52ad5735b8647f37bb4cd3ce9f783
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-13 21:58:43 -0400

    realized my serialization and deserialization was not working, fixed

--- controllers/pet.js
+++ controllers/pet.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     addPetInfo: async (req, res) =>{
         try {
-            await Pet.create({name: req.body.dogName, breed: req.body.dogBreed, birthday: req.body.dogBirthday, gender: req.body.dogGender, spayed: req.body.dogSpay, userId: req.user.id});
+            await Pet.create({name: req.body.dogName, breed: req.body.dogBreed, birthday: req.body.dogBirthday, gender: req.body.dogGender, spayed: req.body.dogSpay, userId: req.user.userId});
             res.redirect('/')
         } catch (error) {
             console.log(error)
