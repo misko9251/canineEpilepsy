36759ebe7b770be3815284598cc358e12a44e802
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-15 21:44:54 -0400

    added post to pet profile page

--- controllers/petProfile.js
+++ controllers/petProfile.js
@@ -1,10 +1,21 @@
 const Pet = require('../models/Pet')
+const Post = require('../models/Post');
 
 module.exports = {
     getProfilePage: async (req, res) =>{
         try {
-            const pet = await Pet.find({userId: req.user.userId})
-            res.render('petProfile.ejs', {myPet: pet, user: req.user})
+            const pet = await Pet.find({userId: req.user.userId});
+            const posts = await Post.find({userId: req.userId});
+            res.render('petProfile.ejs', {myPet: pet, user: req.user, myPosts: posts})
+            console.log(posts.length)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    addPost: async (req, res) =>{
+        try {
+            await Post.create({post: req.body.post})
+            res.redirect('/petProfile')
         } catch (error) {
             console.log(error)
         }
--- routes/petProfile.js
+++ routes/petProfile.js
@@ -4,5 +4,6 @@ const petProfileRouter = require('../controllers/petProfile')
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
 router.get('/', ensureAuth, petProfileRouter.getProfilePage);
+router.post('/addPost', petProfileRouter.addPost);
 
 module.exports = router;
