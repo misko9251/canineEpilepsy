5f17f0db3923884569f63406ef14dffbdaf85483
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-08-21 22:38:41 -0400

    working on microsoft auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const config = require('../config/config');
 const router = express.Router();
 const Pet = require('../models/Pet');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
@@ -7,8 +8,54 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth');
 // Auth with Google with a GET request
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-// Google auth callback
+// Auth with Microsoft with a GET request
+router.get('/login',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect', 
+      { 
+        response: res,                      
+        resourceURL: config.resourceURL,    
+        customState: 'my_state',            
+        failureRedirect: '/' 
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    console.log('Login was called in the Sample');
+    res.redirect('/petEntry');
+});
 
+// Microsoft Reply URL
+router.get('/openid/return',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect', 
+      { 
+        response: res,    
+        failureRedirect: '/'  
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    console.log('We received a return from AzureAD.');
+    res.redirect('/petEntry');
+  });
+
+router.post('/openid/return',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect', 
+      { 
+        response: res,    
+        failureRedirect: '/'  
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    console.log('We received a return from AzureAD.');
+    res.redirect('/petEntry');
+  });
+
+
+// Google auth callback
 router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/' }),
   async function(req, res) {
@@ -21,10 +68,7 @@ router.get('/google/callback',
     }
   });
 
-
-
-// Log Out User
-
+// Google log out user
 router.get('/logout', (req, res) => {
   req.logout((err) => {
     if (err) { return next(err); }
